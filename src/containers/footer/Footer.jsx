7bf64b7c19889a809d5c174ac6f97df16346066e
Ifeanyi Aladi <benedictaladi@gmail.com>
2023-07-15 18:13:57 +0100

    updated the files.

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
-import './footer.css'
+import nftLogo from '../../assets/Logo.svg';
+import './footer.css';
 
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+const Footer = () => (
+  <div className="nft__footer section__padding">
+    <div className="nft__footer-heading">
+      <h1 className="gradient__text">Do you want to step in to the future before others</h1>
+    </div>
 
-export default Footer
+    <div className="nft__footer-btn">
+      <p>Request Early Access</p>
+    </div>
+
+    <div className="nft__footer-links">
+      <div className="nft__footer-links_logo">
+        <img src={nftLogo} alt="nft_logo" />
+        <p>Hetleviktoppen 56, Loddefjord, <br /> All Rights Reserved</p>
+      </div>
+      <div className="nft__footer-links_div">
+        <h4>Links</h4>
+        <p>Overons</p>
+        <p>Social Media</p>
+        <p>Counters</p>
+        <p>Contact</p>
+      </div>
+      <div className="nft__footer-links_div">
+        <h4>Company</h4>
+        <p>Terms & Conditions </p>
+        <p>Privacy Policy</p>
+        <p>Contact</p>
+      </div>
+      <div className="nft__footer-links_div">
+        <h4>Get in touch</h4>
+        <p>Hetleviktoppen 56, Loddefjord</p>
+        <p>+47-472-33-132</p>
+        <p>info@nftcube.com</p>
+      </div>
+    </div>
+
+    <div className="nft__footer-copyright">
+      <p>@2023 NFTcube. All rights reserved.</p>
+    </div>
+  </div>
+);
+
+export default Footer;
--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -8,7 +8,6 @@ const Possibility = () => (
       <img src={nftImage} alt="nft" />
     </div>
     <div className="nft__possibility-content">
-      <h4>Request Early Access to Get Started</h4>
       <h1 className="gradient__text">The possibilities are <br /> beyond your imagination</h1>
       <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
       <h4>Request Early Access to Get Started</h4>
