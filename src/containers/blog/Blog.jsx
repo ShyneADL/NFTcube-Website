de283ce9cc2d6d1232beb5cd42969c6bafab6b55
Ifeanyi Aladi <benedictaladi@gmail.com>
2023-07-16 22:08:06 +0100

    update

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -14,13 +14,13 @@ const Blog = () => (
     </div>
     <div className="nft__blog-container">
       <div className="nft__blog-container_groupA">
-        <Article imgUrl={blog01} date="Jun 12, 2023" text="This week in Web3 and NFTs" />
+        <Article imgUrl={blog01} date="Jul 12, 2023" text="This week in Web3 and NFTs" />
       </div>
       <div className="nft__blog-container_groupB">
-        <Article imgUrl={blog02} date="Sep 26, 2021" text="A Roundup of Yuga Labs' latest developments: Gucci collaboration and expanded collections." />
-        <Article imgUrl={blog03} date="Sep 26, 2021" text="NFT creators diversify into real-world assets to generate new revenues." />
-        <Article imgUrl={blog04} date="Sep 26, 2021" text="NFTs/OpenSea: digital asset prices have further to fall" />
-        <Article imgUrl={blog05} date="Sep 26, 2021" text="The promising future of NFTs remains in a state of flux" />
+        <Article imgUrl={blog02} date="Jul 10, 2023" text="A Roundup of Yuga Labs' latest developments: Gucci collaboration and expanded collections." />
+        <Article imgUrl={blog03} date="Jul 09, 2023" text="NFT creators diversify into real-world assets to generate new revenues." />
+        <Article imgUrl={blog04} date="Jul 07, 2023" text="NFTs/OpenSea: digital asset prices have further to fall" />
+        <Article imgUrl={blog05} date="Jul 06, 2023" text="The promising future of NFTs remains in a state of flux" />
       </div>
     </div>
   </div>
